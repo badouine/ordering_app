63b7fed1864d63a57ecf825ee81e4bf626354047
badouine <nzbadouine@gmail.com>
2024-01-23 16:33:03 +0000

    hero

--- src/app/components/layout/Hero.js
+++ src/app/components/layout/Hero.js
@@ -1,28 +1,35 @@
 import Image from "next/image";
+import Right from "../icons/Right";
 export default function Hero() {
-    return (
-        
-            <section className="grid grid-cols-2">
-               <div>
-               <h1 className="text-4xl font-semibold">
-                    Everything is better with a Pizza
-                </h1>
-                <p className="my-4 text-gray-500">
-                    Pizza is the missing piece that makes every, day complete, a simple yet delicious joy in life
-                </p>
-                <div>
-                    <button className="bg-primary text-white px-8 py-2 rounded-full">Order now</button>
-                    <button>Learn more</button>
-
-                </div>
-               </div>
-            <div className="relative">
-                <Image src={'/pizza.png'} layout={'fill'} 
-                objectFit={'contain'}
-                alt={'pizza'} />
-            </div>
-                                
-            </section>
-        
-    ); 
+  return (
+    <section className="grid grid-cols-2">
+      <div>
+        <h1 className="text-4xl font-semibold">
+          Everything is better with a Pizza
+        </h1>
+        <p className="my-4 text-gray-500">
+          Pizza is the missing piece that makes every, day complete, a simple
+          yet delicious joy in life
+        </p>
+        <div className="flex gap-4">
+          <button className="bg-primary uppercase flex gap-2 text-white px-4 py-2 rounded-full">
+            Order now
+            <Right />
+          </button>
+          <button className="flex gap-2 py-2 text-gray-600 font-semibold">
+            Learn more
+            <Right />
+          </button>
+        </div>
+      </div>
+      <div className="relative">
+        <Image
+          src={"/pizza.png"}
+          layout={"fill"}
+          objectFit={"contain"}
+          alt={"pizza"}
+        />
+      </div>
+    </section>
+  );
 }
