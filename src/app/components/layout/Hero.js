991e7f21c079b88fca0ca8956f0b31618f85c976
badouine <nzbadouine@gmail.com>
2024-04-17 16:46:12 +0000

    section

--- src/app/components/layout/Hero.js
+++ src/app/components/layout/Hero.js
@@ -2,17 +2,17 @@ import Image from "next/image";
 import Right from "../icons/Right";
 export default function Hero() {
   return (
-    <section className="grid grid-cols-2">
-      <div>
+    <section className="hero">
+      <div className="py-12">
         <h1 className="text-4xl font-semibold">
-          Everything is better with a Pizza
+          Everything<br/> is better<br/> with a&nbsp;<span className="text-primary">Pizza</span>
         </h1>
-        <p className="my-4 text-gray-500">
+        <p className="my-6 text-gray-500 text-sm">
           Pizza is the missing piece that makes every, day complete, a simple
           yet delicious joy in life
         </p>
         <div className="flex gap-4">
-          <button className="bg-primary uppercase flex gap-2 text-white px-4 py-2 rounded-full">
+          <button className="bg-primary uppercase flex items-center gap-2 text-white px-4 py-2 rounded-full text-sm">
             Order now
             <Right />
           </button>
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,38 @@
 /* eslint-disable react/jsx-no-undef */
 import Header from './components/layout/Header';
 import Hero from './components/layout/Hero';
+import HomeMenu from './components/layout/HomeMenu';
+import SectionHeaders from './components/layout/Sectionheaders';
 
 
 export default function Home() {
   return (
     <>
-      <Header/>
-      <Hero/>
+      <Header />
+      <Hero />
+      <HomeMenu />
+      <section className='text-center my-16'>
+        <SectionHeaders
+          subHeader={'Our Story'}
+          mainHeader={'About us'}
+        />
+        <div className='text-gray-500 max-w-md mx-auto mt-4 flex flex-col gap-4'>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+        </div>
+        
+      </section>
+      
     </>
   )
 }
