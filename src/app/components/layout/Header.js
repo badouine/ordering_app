94b99d356179fee9bbd84014b9d7fde4cbef5194
badouine <nzbadouine@gmail.com>
2024-04-18 00:52:04 +0000

    footer

--- src/app/components/layout/Header.js
+++ src/app/components/layout/Header.js
@@ -1,15 +1,31 @@
-import Link from "next/link"
+import Link from "next/link";
 export default function Home() {
   return (
-    <header className='flex items-center justify-between '>
-      <Link className="text-red-500 font font-semibold text-2xl" href="">NF PIZZA</Link>
-      <nav className='flex items-center gap-8 text-gray-500 font-semibold'>
-        <Link href={''}>Home</Link>
-        <Link href={''}>Menu</Link>
-        <Link href={''}>About</Link>
-        <Link href={''}>Contact</Link>
-        <Link href={''} className="bg-primary text-white rounded-full px-6 py-2">Login</Link>
+    <header className="flex items-center justify-between ">
+      <Link className="text-red-500 font font-semibold text-2xl" href="">
+        NF PIZZA
+      </Link>
+      <nav className="flex items-center gap-8 text-gray-500 font-semibold">
+        <Link href={""}>Home</Link>
+        <Link href={""}>Menu</Link>
+        <Link href={""}>About</Link>
+        <Link href={""}>Contact</Link>
+        <Link
+          href={""}
+          className="bg-primary text-white rounded-full px-6 py-2"
+        >
+          Login
+        </Link>
+      </nav>
+      <nav>
+        <Link href=""></Link>
+        <Link
+          href={""}
+          className="bg-primary rounded-full text-white px-8 py-2> Login"
+        >
+          Login
+        </Link>
       </nav>
     </header>
-  )
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,8 @@
 /* eslint-disable react/jsx-no-undef */
-import Header from './components/layout/Header';
-import Hero from './components/layout/Hero';
-import HomeMenu from './components/layout/HomeMenu';
-import SectionHeaders from './components/layout/Sectionheaders';
-
+import Header from "./components/layout/Header";
+import Hero from "./components/layout/Hero";
+import HomeMenu from "./components/layout/HomeMenu";
+import SectionHeaders from "./components/layout/Sectionheaders";
 
 export default function Home() {
   return (
@@ -11,28 +10,41 @@ export default function Home() {
       <Header />
       <Hero />
       <HomeMenu />
-      <section className='text-center my-16'>
-        <SectionHeaders
-          subHeader={'Our Story'}
-          mainHeader={'About us'}
-        />
-        <div className='text-gray-500 max-w-md mx-auto mt-4 flex flex-col gap-4'>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+      <section className="text-center my-16">
+        <SectionHeaders subHeader={"Our Story"} mainHeader={"About us"} />
+        <div className="text-gray-500 max-w-md mx-auto mt-4 flex flex-col gap-4">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit
+          </p>
         </div>
-        
       </section>
-      
+      <section className="text-center my-8">
+        <SectionHeaders
+          subHeader={"Don\t' hesitate"}
+          mainHeader={"Contact us"}
+        />
+        <div className="mt-8">
+          <a
+            className="text-4xl underline text-gray-500"
+            href="tel:+2215678990"
+          >
+            +2215678990
+          </a>
+        </div>
+      </section>
+      <footer className="border-t p-8 text-center text-gray-500 mt-16">
+        &copy; All rights reserved
+      </footer>
     </>
-  )
+  );
 }
